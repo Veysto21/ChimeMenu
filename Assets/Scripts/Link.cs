c88132d89a400e3f0c98c2752daa7cd6907c701c
Veysto21 <wstovey@gmail.com>
2023-04-15 14:13:48 -0500

    first Attempt

---
+++ Assets/Scripts/Link.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+
+public class Link : MonoBehaviour
+{
+
+    public void OpenYouTubeChannel()
+    {
+#if !UNITY_EDITOR
+		openWindow("https://www.youtube.com/@hurleypiano3953");
+#endif
+    }
+
+    public void OpenFacebook()
+    {
+#if !UNITY_EDITOR
+		openWindow("https://www.facebook.com/groups/2253683604947579");
+#endif
+    }
+
+    public void OpenInstagram()
+    {
+#if !UNITY_EDITOR
+		openWindow("https://www.instagram.com/chimecandy/");
+#endif
+    }
+
+    public void OpenTwitter()
+    {
+#if !UNITY_EDITOR
+		openWindow("https://twitter.com/HurleyPiano");
+#endif
+    }
+
+    public void OpenTikTok()
+    {
+#if !UNITY_EDITOR
+		openWindow("");
+#endif
+    }
+
+    [DllImport("__Internal")]
+    private static extern void openWindow(string url);
+
+}
